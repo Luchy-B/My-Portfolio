5dd8ed6d561724524e1fc59fc63ef0459df9c8ec
Rana-Mudassir <mduasire38@gmail.com>
2022-11-23 00:34:51 +0500

    Update Mobile Menu li

--- script.js
+++
@@ -1,6 +0,0 @@
-let toggleButton = document.querySelector('.toggle-Icon');
-let navbarLinks = document.querySelector('.dskNav-toggle');
-
-toggleButton.addEventListener('click', () =>{
-    navbarLinks.classList.toggle('active')
-})
