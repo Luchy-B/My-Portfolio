f5d98fde2959499d8965a18ee742bef2e2fe9fd0
Luchy-B <okparabon@gmail.com>
2022-11-25 08:04:18 +0100

    Add Popup window

--- scripts.js
+++ scripts.js
@@ -13,3 +13,28 @@ function hide() {
 
 btn.addEventListener('click', mobileMenu);
 closeX.addEventListener('click', hide);
+
+
+
+function validate(){
+  
+  let mail2 = document.getElementById("email").value;
+
+  let regx = /^([A-Za-z0-9_\-\.\'])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,6})$/;
+
+    if (!mail2.match(regx)) 
+      return false;
+    else 
+  return true;
+
+  // if(regx.mail2){
+  //   alert('You have provided a valid email')
+  //   return true;
+  // }else{
+  //   return false;
+  // }
+    
+}
+
+ // /^([a-z0-9_\-\.]+)@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/;
+  // /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/
